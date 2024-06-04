f5343a99d18fe2b8b95af732b32bbbee700e79f3
H Grasso: PC <aitch.grasso@gmail.com>
2024-06-04 16:18:05 -0500

    type error fix

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,16 +1,16 @@
-// import { minify } from "next/dist/build/swc";
-// import Image from "next/image";
+import { minify } from "next/dist/build/swc";
+import Image from "next/image";
 // import {Item} from './types/index.ts'
 // import Card from "./components/Card.tsx";
-// import data from './data/data.js';
-// import { Cardo } from "next/font/google/index.js";
-// // import me from './assets/profile.png'
-// // import Header from "./components/Header";
-// // import Body from "./components/Body";
-// // import Footer from "./components/Footer";
+import data from './data/data.js';
+import { Cardo } from "next/font/google/index.js";
+// import me from './assets/profile.png'
+// import Header from "./components/Header";
+// import Body from "./components/Body";
+// import Footer from "./components/Footer";
 
 
-// export default function Page() {
+export default function Page() {
 //   const cards = data.map(item => {
 //     return (
 //       <Card 
@@ -20,9 +20,9 @@
 //     )
 //   })
 
-//   return (
-//     <div>
+  return (
+    <div>
 
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
